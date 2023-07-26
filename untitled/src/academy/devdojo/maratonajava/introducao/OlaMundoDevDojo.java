babcd6ef0713908fecdf061055196581041722c3
Ronaldo Dorneles <ronaldodorneles500@gmail.com>
2023-07-26 01:40:09 -0300

    first commit

---
+++ untitled/src/academy/devdojo/maratonajava/introducao/OlaMundoDevDojo.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class OlaMundoDevDojo {
+    //Comentário de uma linha
+    public static void main(String[] args){
+        System.out.println("Olá, Mundo!");
+    }
+}
