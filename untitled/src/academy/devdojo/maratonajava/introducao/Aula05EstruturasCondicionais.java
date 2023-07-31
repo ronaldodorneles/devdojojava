7e23a09f88337ed046e908fdaf69c018aa7b747a
dead <dead@pop-os.localdomain>
2023-07-31 01:30:44 -0300

    Adicionados aula 4 e 5

---
+++ untitled/src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 15;
+
+        if (idade >= 18) {
+            System.out.println("Autorizado a comprar bebida alcólica.");
+        }
+        System.out.println("Fora do if");
+    }
+}
