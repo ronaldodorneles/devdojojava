191eaf856d5e84950f3481a799f440993451beff
dead <dead@pop-os.localdomain>
2023-08-02 21:51:01 -0300

    Adicionada aula 8

--- untitled/src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ untitled/src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -13,9 +13,14 @@ public class Aula08ArraysMultidimensionais01 {
         dias[1][1] = 28;
         dias[1][2] = 31;
 
-        for(int i=0; i < dias.length; i++){
-            for (int j=0; j< dias[0].length ; j++){
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[i].length; j++) {
                 System.out.println(dias[i][j]);
+            }
+        }
+        for (int[] arrBase : dias) {
+            for (int num : arrBase) {
+                System.out.println(num);
             }
         }
 
