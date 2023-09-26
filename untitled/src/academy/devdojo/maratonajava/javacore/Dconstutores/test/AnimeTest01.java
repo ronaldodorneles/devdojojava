435e47dd872e7235e89554edc4d30bf20835a1f2
dead <dead@pop-os.localdomain>
2023-09-26 20:08:33 -0300

    Adicionado até construtores

---
+++ untitled/src/academy/devdojo/maratonajava/javacore/Dconstutores/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Dconstutores.test;
+
+
+import academy.devdojo.maratonajava.javacore.Dconstutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("InuYasha", "TV", 152, "Aventura, ação", "Sunrise");
+        anime.imprime();
+
+    }
+}
